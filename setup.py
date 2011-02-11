e30c64ce47da4f1de1989f058ec8ad298d4b2e34
stark <fredrik.stark@mazemedia.se>
2011-02-11 16:58:22 +0100

    first commit

---
+++ setup.py
@@ -0,0 +1,10 @@
+#!/usr/bin/env python
+
+from setuptools import setup, find_packages
+
+setup(name='sforce',
+      version='0.0.0.1',
+      description='I hate SalesForce',
+      author='Noone',
+      author_email='noone@example.com',
+      packages=['sforce'],)
